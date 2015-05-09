2427bc6ec91a0e386139f8c33ba7157f12ece025
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-05-09 16:43:43 -0700

    add db entry for new crawls

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -3,7 +3,7 @@ class CrawlsController < ApplicationController
   end
 
 	def search
-  	parameters = { limit: 10,
+  	parameters = { limit: 5,
   					sort: 1,
   					category_filter: "bars",
   					radius_filter: 10000 }
@@ -20,8 +20,30 @@ class CrawlsController < ApplicationController
   def create
     #byebug
 
-    @locations = params.require(:locations)[:bars]
+    @user = current_user
+
+    @results = params.require(:locations)[:bars]
+    @name = params.require(:locations).permit(:crawl)["crawl"]
+
+    @all_locations = []
+    city = ""
+    crawlAddress = @results[0].split(" | ")
+    crawlCity = crawlAddress[1].split(", ")
+    @crawl = @user.crawls.create(name: @name, city: crawlCity[1])
+    @results.each do |x| 
+      temp = []
+      stuff = x.split(" | ")
+      city = stuff[1].split(", ")[1]
+      temp << stuff[0] #bar id
+      temp << stuff[1] #address
+      temp << stuff[2] #bar name
+      temp << stuff[3].to_f #lat
+      temp << stuff[4].to_f #lng
+      @all_locations << temp
+      @crawl.locations << Location.find_or_create_by(address: stuff[1], lat: stuff[3].to_f, lng: stuff[4].to_f, name: stuff[2], business_id: stuff[0])
+    end
 
     render :crawl
   end
 end
+
