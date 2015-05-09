f76dac4cdc437f4544970f8fcf658a4b62073f2a
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-05-09 13:49:57 -0700

    add jquery gem and script tag

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'devise'
 
 gem 'yelp'
 
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+$(function () {
+
+  // grab the `search-res`
+  var $searchRes = $("#search-res");
