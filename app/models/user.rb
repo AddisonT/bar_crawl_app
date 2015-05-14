034d00f413aafbbcc7883db003aaf99bc105812c
AddisonT <addisontam1@Addisons-MacBook-Pro.local>
2015-05-14 09:12:48 -0700

    added name validation to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
+
   has_many :crawls
 end
